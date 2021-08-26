76c539a6f16b539b09a4acbb733d24acb3319d55
marcin.madejski91@gmail.com <marcin.madejski91@gmail.com>
2021-08-26 21:30:50 +0200

    init

---
+++ index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Restauracja</title>	
+		<meta charset="UTF-8">
+		<meta name=viewport content="width=device-width, initial-scale=1">		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<script defer src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+		<script defer src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script defer src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	    <script type="text/javascript" src="js/scripts.min.js?ver=1"></script>	
+		<link href="https://fonts.googleapis.com/css?family=Creepster" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Roboto:300i,400,700" rel="stylesheet">
+		<link rel="stylesheet" href="css/style.css?ver=1" type="text/css">	
+	</head>
+	<body>	
+		<div class="container border-style">
+
+			<header class="header-content">
+				<h3 class="uppercase white-color">Tonio's bar & grill</h3>
+				<h1 class="white-color">SPOOKY<br>NIGHT</h1>
+				<h3 class="uppercase white-color">Drink specials</h3>
+				<div class="header-content__ghost-boby"></div>
+				<div class="header-content__ghost-casper"></div>
+				<div class="header-content__ghost-meghan"></div>
+			</header>
+
+			<section class="menu-section">
+				<div class="row">
+
+					<div class="menu-section__image order-2 order-lg-1 order-xl-1">
+						<div class="menu-section__image--spider"></div>
+					</div>
+					<div class="menu-section__card order-1 order-lg-2 order-xl-2">
+						<div class="row">
+							<div class="col-12 col-lg-10 col-xl-12 pt-3">
+								<div class="row">
+									<div class="col-10 uppercase bold">
+										Vampire dekught
+									</div>
+									<div class="col-2 bold">
+									7$
+									</div>
+									<div class="col-10 italic light">
+										rasperry vodka, pinaple juice, lime juice
+									</div>
+								</div>
+							</div>
+							<div class="col-12 col-lg-10 col-xl-12 pt-3">
+								<div class="row">
+									<div class="col-10 uppercase bold">
+										Blood mary
+									</div>
+									<div class="col-2 bold">
+									6$
+									</div>
+									<div class="col-10 italic light">
+										rasperry vodka, pinaple juice, lime juice
+									</div>
+								</div>
+							</div>
+							<div class="col-12 col-lg-10 col-xl-12 pt-3">
+								<div class="row">
+									<div class="col-10 uppercase bold">
+										Blue devil
+									</div>
+									<div class="col-2 bold">
+									20$
+									</div>
+									<div class="col-10 italic light">
+										rasperry vodka, pinaple juice, lime juice
+									</div>
+								</div>
+							</div>
+							<div class="col-12 col-lg-10 col-xl-12 pt-3">
+								<div class="row">
+									<div class="col-10 uppercase bold">
+										Zombie
+									</div>
+									<div class="col-2 bold">
+									17$
+									</div>
+									<div class="col-10 italic light">
+										rasperry vodka, pinaple juice, lime juice
+									</div>
+								</div>
+							</div>
+							<div class="col-12 col-lg-10 col-xl-12 pt-3">
+								<div class="row">
+									<div class="col-10 uppercase bold">
+										The frankenstein
+									</div>
+									<div class="col-2 bold">
+									2$
+									</div>
+									<div class="col-10 italic light">
+										rasperry vodka, pinaple juice, lime juice
+									</div>
+								</div>
+							</div>
+							<div class="col-12 col-lg-10 col-xl-12 pt-3">
+								<div class="row">
+									<div class="col-10 uppercase bold">
+										Silver Sword
+									</div>
+									<div class="col-2 bold">
+									10$
+									</div>
+									<div class="col-10 italic light">
+										rasperry vodka, pinaple juice, lime juice
+									</div>
+								</div>
+							</div>
+							<div class="col-12 col-lg-10 col-xl-12 pt-3">
+								<div class="row">
+									<div class="col-10 uppercase bold">
+										Dracula
+									</div>
+									<div class="col-2 bold">
+									20$
+									</div>
+									<div class="col-10 italic light">
+										rasperry vodka, pinaple juice, lime juice
+									</div>
+								</div>
+							</div>
+							<div class="col-12 col-lg-10 col-xl-12 pt-3">
+								<div class="row">
+									<div class="col-10 uppercase bold">
+										Dragon tail
+									</div>
+									<div class="col-2 bold">
+									39$
+									</div>
+									<div class="col-10 italic light">
+										rasperry vodka, pinaple juice, lime juice
+									</div>
+								</div>
+							</div>
+							<div class="col-12 col-lg-10 col-xl-12 pt-3">
+								<div class="row">
+									<div class="col-10 uppercase bold">
+										Big Shot
+									</div>
+									<div class="col-2">
+									28$
+									</div>
+									<div class="col-10 italic light">
+										rasperry vodka, pinaple juice, lime juice
+									</div>
+								</div>
+							</div>
+							<div class="col-12 col-lg-10 col-xl-12 pt-3">
+								<div class="row">
+									<div class="col-10 uppercase bold">
+										Blood
+									</div>
+									<div class="col-2 bold">
+									24$
+									</div>
+									<div class="col-10 italic light">
+										rasperry vodka, pinaple juice, lime juice
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<footer>
+				<span>
+					<!-- &copy; <?php echo date('Y'); ?> All rights reserved by Marcin Madejski -->
+				</span>
+			</footer>
+		</div>
+	</body>
+</html>
